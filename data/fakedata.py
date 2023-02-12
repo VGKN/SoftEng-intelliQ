086719ab755ab1eb056882ae523dd004353401a0
Konstantinos Vougias <el19144@mail.ntua.gr>
2023-02-12 17:23:42 +0200

    fixed escape character

--- data/fakedata.py
+++ data/fakedata.py
@@ -2,7 +2,7 @@ import json
 import random
 import string
 
-"""
+
 with open('new.json', encoding='utf-8') as file:
     data=json.load(file)
 Keywords=[]
@@ -46,20 +46,20 @@ for questions in data['questions']:
                         texts.append(options['opttxt'])
         print(myx,myy)
         print(texts)
-        strhelp='/'
-        questiontext=questions['qtext'][0:myx[0]]+strhelp+texts[1]+strhelp+questions['qtext'][myy[0]+1:myx[1]] +strhelp+texts[0]+strhelp+questions['qtext'][myy[1]+1:]          
-       
+        #strhelp='\''
+        questiontext=questions['qtext'][0:myx[0]]+"\\'"+texts[1]+"\\'"+questions['qtext'][myy[0]+1:myx[1]] +"\\'"+texts[0]+"\\'"+questions['qtext'][myy[1]+1:]          
+        print(questiontext)
         L.append("INSERT INTO Question (Question_ID, Qtext, Qrequired, Qtype, QuestionaireID) VALUES ('{}','{}','{}','{}','{}');".format(questions['qID '],questiontext,questions['required'],questions['type'],data['questionnaireID']))
     else:
         L.append("INSERT INTO Question (Question_ID, Qtext, Qrequired, Qtype, QuestionaireID) VALUES ('{}','{}','{}','{}','{}');".format(questions['qID '],questions['qtext'],questions['required'],questions['type'],data['questionnaireID']))
-    for option in questions['options']:
+    """for option in questions['options']:
        X.append("INSERT INTO Options (Opt_ID, Opt_Text) VALUES ('{}','{}');".format(option['optID'], option['opttxt']))
        if option['nextqID']=='-':
             nextq=questions['qID ']
        else:
             nextq=option['nextqID']
        Z.append("INSERT INTO Questions_Options (QuestionID, OptID, Next_Q) VALUES ('{}','{}','{}');".format(questions['qID '], option['optID'], nextq))
-"""
+
 s = string.ascii_letters
 for _ in range(10):
     c = ''.join(random.choice(s) for _ in range(8))
@@ -67,20 +67,20 @@ for _ in range(10):
 
 L=[]
 Z=[]
-with open('new9.json', encoding='utf-8') as f:
-    data2=json.load(f)
-    L.append("INSERT INTO Sesion (Session_ID, QuestionnaireID, UserString) VALUES ('{}','{}','{}');".format(data2['session'],data2['questionnaireID'],c))
-    for answer in data2['answers']:
-        Z.append("INSERT INTO Session_Questions_Options (Q_ID, S_ID, O_ID) VALUES ('{}','{}','{}');".format(answer['qID '],data2['session'],answer['ans']))
+#with open('new9.json', encoding='utf-8') as f:
+   # data2=json.load(f)
+    #L.append("INSERT INTO Sesion (Session_ID, QuestionnaireID, UserString) VALUES ('{}','{}','{}');".format(data2['session'],data2['questionnaireID'],c))
+   # for answer in data2['answers']:
+    #    Z.append("INSERT INTO Session_Questions_Options (Q_ID, S_ID, O_ID) VALUES ('{}','{}','{}');".format(answer['qID '],data2['session'],answer['ans']))
 
-   
+   """
 with open('DML2.sql', 'a') as file:
     for line in L:
         file.write(line)
         file.write('\n')
-    #for line in X:
-        #file.write(line)
-        #file.write('\n')
+    for line in X:
+        file.write(line)
+        file.write('\n')
     for line in Z:
         file.write(line)
         file.write('\n')
