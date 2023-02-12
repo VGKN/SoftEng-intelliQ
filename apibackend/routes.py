12e84abad02e02c04a0ca1ce7f24176df68a9b57
Kassaris Nikolaos <el19188@mail.ntua.gr>
2023-02-12 15:14:03 +0200

    organized routes

--- apibackend/routes.py
+++ apibackend/routes.py
@@ -14,37 +14,6 @@ def index():
         print(e)
         abort(500)
 
-#redirection upon successfull upload of the allowed files
-@app.route('/success', methods = ['GET'])  
-def success():  
-    if request.method == 'GET': 
-       return render_template("Acknowledgement.html")
-    
-#process of file upload in /questionnaire_upd
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/questionnaire_upd', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            print(filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('success', name=filename))
-    return render_template("questionnaire_upd.html",pageTitle="Upload Questionnaire")
-
 @app.route("/user")
 def getUser():
     try:
@@ -115,6 +84,38 @@ def getOrgs():
     except Exception as e:
         print(e)
         return render_template("base.html",pageTitle="Landing Page")
+    
+#redirection upon successfull upload of the allowed files
+@app.route('/success', methods = ['GET'])  
+def success():  
+    if request.method == 'GET': 
+       return render_template("Acknowledgement.html")
+    
+#process of file upload in /questionnaire_upd
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/questionnaire_upd', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('success', name=filename))
+    return render_template("questionnaire_upd.html",pageTitle="Upload Questionnaire")
+
 
 @app.route("/healthcheck", methods=["GET"])
 def getStatus():
