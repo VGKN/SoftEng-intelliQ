31603ca0dfac6d23b01cc28d6d8379ac9f87b566
Konstantinos Vougias <el19144@mail.ntua.gr>
2023-02-16 21:03:38 +0200

    test

---
+++ cli/test_dbopen.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+from pathlib import Path
+
+def capture(command):
+	proc = subprocess.Popen(command,
+		stdout = subprocess.PIPE,
+		stderr = subprocess.PIPE,
+	)
+	out,err = proc.communicate()
+	return out, err, proc.returncode
