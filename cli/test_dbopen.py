3b1982da812a4d43f99ae42d620513dbdeb4ddae
Konstantinos Vougias <el19144@mail.ntua.gr>
2023-02-16 22:10:19 +0200

    figured out pytest

--- cli/test_dbopen.py
+++ cli/test_dbopen.py
@@ -1,11 +1,14 @@
 import subprocess
 import os
+import sys
 from pathlib import Path
 
 def capture(command):
-	proc = subprocess.Popen(command,
-		stdout = subprocess.PIPE,
-		stderr = subprocess.PIPE,
-	)
-	out,err = proc.communicate()
-	return out, err, proc.returncode
+    proc = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out,err = proc.communicate()
+    return out, err, proc.returncode
+    
+def test_healthcheck():
+    command = [sys.executable,'healthcheck']
+    out, err, exit_code= capture(command)
+    assert b"200" in out
