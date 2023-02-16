8a8500757dde1a636607fcd73a58e04c053f6196
Stefanos Theos <el19219@mail.ntua.gr>
2023-02-16 22:30:46 +0200

    template for tests

--- cli/test_dbopen.py
+++ cli/test_dbopen.py
@@ -1,11 +1,40 @@
 import subprocess
 import os
+import sys
 from pathlib import Path
 
 def capture(command):
-	proc = subprocess.Popen(command,
-		stdout = subprocess.PIPE,
-		stderr = subprocess.PIPE,
-	)
-	out,err = proc.communicate()
-	return out, err, proc.returncode
+    proc = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out,err = proc.communicate()
+    return out, err, proc.returncode
+    
+def test_healthcheck():
+    command = [sys.executable,'healthcheck']
+    out, err, exit_code= capture(command)
+    assert b"200" in out
+
+def test_doanswer():
+    pass
+    
+def test_questionnaire():
+    pass
+    
+def test_question():
+    pass
+    
+def test_questionnaire_upd():
+    pass
+    
+def test_resetall():  
+	command = [sys.executable,'resetall']
+    out, err, exit_code= capture(command)
+    assert b"200" in out:
+    
+def test_resetq():
+    pass
+    
+def test_getquestionanswers():
+    pass
+ 
+def test_getsessionanswers():
+    pass
