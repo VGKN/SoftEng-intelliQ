b7a002cc8127ad9bb9ac3384d61b43cd6484b4d3
Konstantinos Vougias <el19144@mail.ntua.gr>
2023-02-17 02:42:29 +0200

    moved testing to proper folder

--- test/test_admin.py
+++ test/test_admin.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 
 def capture(command):
-    proc = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    proc = subprocess.Popen(command,cwd='../cli', stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     out,err = proc.communicate()
     return out, err, proc.returncode
     
