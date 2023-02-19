0baf91d4d171ac587bed2c323bfc73ff8ef95713
Konstantinos Vougias <el19144@mail.ntua.gr>
2023-02-19 02:10:42 +0200

    final

--- cli/cli-script.sh
+++ cli/cli-script.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-python healthcheck --format json
-python questionnaire --questionnaire_id QQ000 --format json
-python question --questionnaire_id QQ000 --question_id P01 --format json
-python doanswer --questionnaire_id QQ000 --question_id P01 --session_id zzzz --option_id P01A1 --format json
-python question --questionnaire_id QQ000 --question_id Q01 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q01 --session_id zzzz --option_id Q01A1 --format json
-python question --questionnaire_id QQ000 --question_id Q02 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q02 --session_id zzzz --option_id Q02A1 --format json
-python question --questionnaire_id QQ000 --question_id Q03 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q03 --session_id zzzz --option_id Q03A1 --format json
-python question --questionnaire_id QQ000 --question_id Q04 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q04 --session_id zzzz --option_id Q04A2 --format json
-python question --questionnaire_id QQ000 --question_id Q06 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q06 --session_id zzzz --option_id Q06A2 --format json
-python question --questionnaire_id QQ000 --question_id Q07 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q07 --session_id zzzz --option_id Q07A2 --format json
-python question --questionnaire_id QQ000 --question_id Q08 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q08 --session_id zzzz --option_id Q08A1 --format json
-python question --questionnaire_id QQ000 --question_id Q09 --format json
-python doanswer --questionnaire_id QQ000 --question_id Q09 --session_id zzzz --option_id Q09A1 --format json
-python getquestionanswers --questionnaire_id QQ000 --question_id Q01 --format json
-python resetall --format json
-python questionnaire_upd --source test_q2.json --format json
-python questionnaire --questionnaire_id QQ000 --format json
+./se2219 healthcheck --format json
+./se2219 questionnaire --questionnaire_id QQ000 --format json
+./se2219 question --questionnaire_id QQ000 --question_id P01 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id P01 --session_id zzzz --option_id P01A1 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q01 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q01 --session_id zzzz --option_id Q01A1 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q02 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q02 --session_id zzzz --option_id Q02A1 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q03 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q03 --session_id zzzz --option_id Q03A1 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q04 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q04 --session_id zzzz --option_id Q04A2 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q06 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q06 --session_id zzzz --option_id Q06A2 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q07 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q07 --session_id zzzz --option_id Q07A2 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q08 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q08 --session_id zzzz --option_id Q08A1 --format json
+./se2219 question --questionnaire_id QQ000 --question_id Q09 --format json
+./se2219 doanswer --questionnaire_id QQ000 --question_id Q09 --session_id zzzz --option_id Q09A1 --format json
+./se2219 getquestionanswers --questionnaire_id QQ000 --question_id Q01 --format json
+./se2219 resetall --format json
+./se2219 questionnaire_upd --source test_q2.json --format json
+./se2219 questionnaire --questionnaire_id QQ000 --format json
